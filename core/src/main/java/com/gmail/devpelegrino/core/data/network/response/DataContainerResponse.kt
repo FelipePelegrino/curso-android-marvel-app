803e61cb72a8d66a33d044647b41a50cff1bd6de
Felipe Pelegrino <devpelegrino@gmail.com>
2022-11-21 16:18:46 -0300

    Mapeamento dos objetos de response da api da marvel

---
+++ core/src/main/java/com/gmail/devpelegrino/core/data/network/response/DataContainerResponse.kt
@@ -0,0 +1,5 @@
+package com.gmail.devpelegrino.core.data.network.response
+
+data class DataContainerResponse(
+    val results: List<CharacterResponse>
+)
---
+++ core/src/main/java/com/gmail/devpelegrino/core/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,6 @@
+package com.gmail.devpelegrino.core.data.network.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extension: String
+)
