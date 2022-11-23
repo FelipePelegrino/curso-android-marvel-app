f7e9f1a020e707b0a9af767975876b6bdd82f403
Felipe Pelegrino <devpelegrino@gmail.com>
2022-11-23 15:56:53 -0300

    Implementa characters paging source com injeção de dependencia

--- app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/example/marvelapp/framework/CharactersRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.marvelapp.framework
 
 import androidx.paging.PagingSource
 import com.example.marvelapp.framework.network.response.DataWrapperResponse
+import com.example.marvelapp.framework.paging.CharactersPagingSource
 import com.gmail.devpelegrino.core.data.repository.CharactersRemoteDataSource
 import com.gmail.devpelegrino.core.data.repository.CharactersRepository
 import com.gmail.devpelegrino.core.domain.model.Character
@@ -12,6 +13,6 @@ class CharactersRepositoryImpl @Inject constructor(
 ) : CharactersRepository {
 
     override fun getCharacters(query: String): PagingSource<Int, Character> {
-        TODO("Not yet implemented")
+        return CharactersPagingSource(remoteDataSource, query)
     }
 }
