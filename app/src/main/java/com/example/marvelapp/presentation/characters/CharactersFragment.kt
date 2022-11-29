9a4e2d519b6f9e8bf0f67f4552a09229ec4b116c
Felipe Pelegrino <devpelegrino@gmail.com>
2022-11-28 21:57:29 -0300

    Trata estados de loading e error

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -24,7 +29,7 @@ class CharactersFragment : Fragment() {
     * */
     private val viewModel: CharactersViewModel by viewModels()
 
-    private val charactersAdapter = CharactersAdapter()
+    private lateinit var charactersAdapter: CharactersAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,18 +41,79 @@ class CharactersFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initCharactersAdapter()
+        observeInitialLoadState()
+        observeFlow()
+    }
 
+    /*
+    * lifecycleScope.launch pode causar crash, pois quando o app vai para background ele continua
+    * coletando o flow, e pode ser que ao tentar atualizar o adapter, ele não esteja instanciado
+    * por estar em background, ocasionando um erro, para isso, a solução é:
+    * viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+    *     ....
+    * }
+    * Quando for para background, ele automaticamente pausa o flow, e ao voltar no lifecycle onStart
+    * ele volta a escutar o flow
+    * */
+    private fun observeFlow() {
         lifecycleScope.launch {
-            viewModel.charactersPagingData("").collect { pagingData ->
-                charactersAdapter.submitData(pagingData)
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.charactersPagingData("").collect { pagingData ->
+                    charactersAdapter.submitData(pagingData)
+                }
             }
         }
     }
 
     private fun initCharactersAdapter() {
+        charactersAdapter = CharactersAdapter()
         with(binding.recyclerCharacters) {
+            scrollToPosition(0)
             setHasFixedSize(true)
-            adapter = charactersAdapter
+            adapter = charactersAdapter.withLoadStateFooter(
+                footer = CharactersLoadMoreStateAdapter(charactersAdapter::retry)
+            )
         }
+    }
+
+    private fun observeInitialLoadState() {
+        lifecycleScope.launch {
+            charactersAdapter.loadStateFlow.collectLatest { loadState ->
+                binding.flipperCharacters.displayedChild = when (loadState.refresh) {
+                    is LoadState.Loading -> {
+                        setShimmerVisibility(true)
+                        FLIPPER_CHILD_LOADING
+                    }
+                    is LoadState.NotLoading -> {
+                        setShimmerVisibility(false)
+                        FLIPPER_CHILD_CHARACTERS
+                    }
+                    is LoadState.Error -> {
+                        setShimmerVisibility(false)
+                        binding.includeViewCharactersErrorState.buttonRetry.setOnClickListener {
+                            charactersAdapter.refresh()
+                        }
+                        FLIPPER_CHILD_ERROR
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setShimmerVisibility(visibility: Boolean) {
+        binding.includeViewCharactersLoadingState.shimmerCharacters.run {
+            isVisible = visibility
+            if (visibility) {
+                startShimmer()
+            } else {
+                stopShimmer()
+            }
+        }
+    }
+
+    companion object {
+        private const val FLIPPER_CHILD_LOADING = 0
+        private const val FLIPPER_CHILD_CHARACTERS = 1
+        private const val FLIPPER_CHILD_ERROR = 2
     }
 }
---
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersLoadMoreStateViewHolder.kt
@@ -0,0 +1,36 @@
+package com.example.marvelapp.presentation.characters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marvelapp.databinding.ItemCharacterLoadMoreStateBinding
+
+class CharactersLoadMoreStateViewHolder(
+    itemCharacterLoadMoreStateBinding: ItemCharacterLoadMoreStateBinding,
+    retry: () -> Unit
+) : RecyclerView.ViewHolder(itemCharacterLoadMoreStateBinding.root) {
+
+    private val binding = ItemCharacterLoadMoreStateBinding.bind(itemView)
+    private val progressBarLoadingMore = binding.progressLoadMore
+    private val textTryAgainMessage = binding.textTryAgain.also {
+        it.setOnClickListener { retry() }
+    }
+
+    fun bind(loadState: LoadState) {
+        progressBarLoadingMore.isVisible = loadState is LoadState.Loading
+        textTryAgainMessage.isVisible = loadState is LoadState.Error
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, retry: () -> Unit): CharactersLoadMoreStateViewHolder {
+            val itemBinding = ItemCharacterLoadMoreStateBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+            return CharactersLoadMoreStateViewHolder(itemBinding, retry)
+        }
+    }
+}
