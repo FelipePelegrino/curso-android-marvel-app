8fff85edce1b43dd393fbd74f8818b030b8b39ec
Felipe Pelegrino <devpelegrino@gmail.com>
2022-12-07 10:45:48 -0300

    Configura qualifiers para injeção de dependencia | Configura launch fragment com hilt para teste de UI | Configura network security config para permitir localhost que sera usado pelo MockWebServer

---
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -0,0 +1,40 @@
+package com.example.marvelapp.presentation.characters
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.marvelapp.R
+import com.example.marvelapp.framework.di.BaseUrlModule
+import com.example.marvelapp.launchFragmentInHiltContainer
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@UninstallModules(BaseUrlModule::class)
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class CharactersFragmentTest {
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun setUp() {
+        launchFragmentInHiltContainer<CharactersFragment>()
+    }
+
+    @Test
+    fun shouldShowCharactersWhenViewIsCreated() {
+        onView(
+            withId(R.id.recycler_characters)
+        ).check(
+            matches(isDisplayed())
+        )
+    }
+}
