067e27a42e45251040a12ed2e6bb8f225911fcff
Felipe Pelegrino <devpelegrino@gmail.com>
2022-12-14 18:29:06 -0300

    Implementa unitTests detailViewModel completo

--- app/src/test/java/com/example/marvelapp/presentation/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/example/marvelapp/presentation/detail/DetailViewModelTest.kt
@@ -2,15 +2,17 @@ package com.example.marvelapp.presentation.detail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.core.domain.model.Comic
 import com.example.core.usecase.GetCharacterCategoriesUseCase
 import com.example.core.usecase.base.ResultStatus
 import com.example.marvelapp.R
-import com.example.marvelapp.getOrAwaitValue
 import com.example.testing.MainCoroutineRule
 import com.example.testing.model.CharacterFactory
 import com.example.testing.model.ComicFactory
 import com.example.testing.model.EventFactory
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.isA
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
@@ -81,50 +83,105 @@ class DetailViewModelTest {
             detailViewModel.getCharacterCategories(character.id)
 
             // Assert
-            val expected = DetailViewModel.UiState.Success(
-                listOf(
-                    DetailParentVE(
-                        categoryStringResId = R.string.details_comics_category,
-                        detailChildList = listOf(
-                            DetailChildVE(
-                                comics[0].id,
-                                comics[0].imageUrl
-                            )
-                        )
-                    ),
-                    DetailParentVE(
-                        categoryStringResId = R.string.details_events_category,
-                        detailChildList = listOf(
-                            DetailChildVE(
-                                events[0].id,
-                                events[0].imageUrl
-                            )
-                        )
-                    )
-                )
+            verify(uiStateObserver).onChanged(isA<DetailViewModel.UiState.Success>())
+
+            val uiStateSuccess = detailViewModel.uiState.value as DetailViewModel.UiState.Success
+            val categoriesParentList = uiStateSuccess.detailParentList
+
+            assertEquals(2, categoriesParentList.size)
+            assertEquals(
+                R.string.details_comics_category,
+                categoriesParentList[0].categoryStringResId
             )
-            val result = detailViewModel.uiState.getOrAwaitValue()
-            assertEquals(expected, result)
+            assertEquals(
+                R.string.details_events_category,
+                categoriesParentList[1].categoryStringResId
+            )
         }
 
     @Test
     fun `should notify uiState with Success from UiState when get character categories returns only comics`() {
-        // TODO: Implement tests
+        // Arrange
+        whenever(getCharacterCategoriesUseCase.invoke(any()))
+            .thenReturn(
+                flowOf(
+                    ResultStatus.Success(
+                        comics to emptyList()
+                    )
+                )
+            )
+
+        // Act
+        detailViewModel.getCharacterCategories(character.id)
+
+        // Assert
+        verify(uiStateObserver).onChanged(isA<DetailViewModel.UiState.Success>())
+
+        val uiStateSuccess = detailViewModel.uiState.value as DetailViewModel.UiState.Success
+        val categoriesParentList = uiStateSuccess.detailParentList
+
+        assertEquals(1, categoriesParentList.size)
+        assertEquals(R.string.details_comics_category, categoriesParentList[0].categoryStringResId)
     }
 
     @Test
     fun `should notify uiState with Success from UiState when get character categories returns only events`() {
-        // TODO: Implement tests
+        // Arrange
+        whenever(getCharacterCategoriesUseCase.invoke(any()))
+            .thenReturn(
+                flowOf(
+                    ResultStatus.Success(
+                        emptyList<Comic>() to events
+                    )
+                )
+            )
+
+        // Act
+        detailViewModel.getCharacterCategories(character.id)
+
+        // Assert
+        verify(uiStateObserver).onChanged(isA<DetailViewModel.UiState.Success>())
+
+        val uiStateSuccess = detailViewModel.uiState.value as DetailViewModel.UiState.Success
+        val categoriesParentList = uiStateSuccess.detailParentList
+
+        assertEquals(1, categoriesParentList.size)
+        assertEquals(R.string.details_events_category, categoriesParentList[0].categoryStringResId)
     }
 
     @Test
     fun `should notify uiState with Empty from UiState when get character categories returns an empty result list`() {
-        // TODO: Implement tests
+        // Arrange
+        whenever(getCharacterCategoriesUseCase.invoke(any()))
+            .thenReturn(
+                flowOf(
+                    ResultStatus.Success(
+                        emptyList<Comic>() to emptyList()
+                    )
+                )
+            )
+
+        // Act
+        detailViewModel.getCharacterCategories(character.id)
+
+        // Assert
+        verify(uiStateObserver).onChanged(isA<DetailViewModel.UiState.Empty>())
     }
 
     @Test
     fun `should notify uiState with Error from UiState when get character categories returns an exception`() {
-        // TODO: Implement tests
-    }
+        // Arrange
+        whenever(getCharacterCategoriesUseCase.invoke(any()))
+            .thenReturn(
+                flowOf(
+                    ResultStatus.Error(RuntimeException())
+                )
+            )
 
+        // Act
+        detailViewModel.getCharacterCategories(character.id)
+
+        // Assert
+        verify(uiStateObserver).onChanged(isA<DetailViewModel.UiState.Error>())
+    }
 }
