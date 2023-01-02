2df080e35eb499c10289d0d595a89d69f5f0fa97
Felipe Pelegrino <devpelegrino@gmail.com>
2023-01-02 11:52:55 -0300

    Atualizando UI Tests, dependencias e api de menu

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,5 +1,7 @@
 package com.example.marvelapp.presentation.characters
 
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
@@ -16,6 +18,8 @@ import com.example.marvelapp.presentation.characters.adapters.CharactersViewHold
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
@@ -34,18 +38,25 @@ class CharactersFragmentTest {
 
     private lateinit var server: MockWebServer
 
+    private val navController = TestNavHostController(
+        ApplicationProvider.getApplicationContext()
+    )
+
     @Before
     fun setUp() {
         server = MockWebServer().apply {
             start(8080)
         }
-        launchFragmentInHiltContainer<CharactersFragment>()
+        launchFragmentInHiltContainer<CharactersFragment>(
+            navHostController = navController
+        )
     }
 
     @Test
-    fun shouldShowCharacters_whenViewIsCreated() {
+    fun shouldShowCharacters_whenViewIsCreated(): Unit = runBlocking {
         server.enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
 
+        delay(500)
         onView(
             withId(R.id.recycler_characters)
         ).check(
@@ -54,7 +65,7 @@ class CharactersFragmentTest {
     }
 
     @Test
-    fun shouldLoadMoreCharacters_whenNewPageIsRequested() {
+    fun shouldLoadMoreCharacters_whenNewPageIsRequested(): Unit = runBlocking {
         // Arrange
         with(server) {
             enqueue(MockResponse().setBody("characters_p1.json".asJsonString()))
@@ -62,6 +73,7 @@ class CharactersFragmentTest {
         }
 
 
+        delay(1000)
         // Action
         onView(
             withId(R.id.recycler_characters)
@@ -78,10 +90,11 @@ class CharactersFragmentTest {
     }
 
     @Test
-    fun shouldShowErrorView_whenReceivesAnErrorFromApi() {
+    fun shouldShowErrorView_whenReceivesAnErrorFromApi(): Unit = runBlocking {
         // Arrange
         server.enqueue(MockResponse().setResponseCode(404))
 
+        delay(1000)
         onView(
             withId(R.id.text_initial_loading_error)
         )
--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuProvider
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -33,7 +34,8 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class CharactersFragment : Fragment(),
     SearchView.OnQueryTextListener,
-    MenuItem.OnActionExpandListener {
+    MenuItem.OnActionExpandListener,
+    MenuProvider {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding get() = _binding!!
@@ -77,6 +79,9 @@ class CharactersFragment : Fragment(),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         initCharactersAdapter()
         observeInitialLoadState()
         viewModel.searchCharacters()
@@ -84,13 +89,8 @@ class CharactersFragment : Fragment(),
         observeSortingData()
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.characters_menu_items, menu)
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.characters_menu_items, menu)
 
         val searchItem = menu.findItem(R.id.menu_search)
         searchView = searchItem.actionView as SearchView
@@ -108,13 +108,13 @@ class CharactersFragment : Fragment(),
         }
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when (menuItem.itemId) {
             R.id.menu_sort -> {
                 findNavController().navigate(R.id.action_charactersFragment_to_sortFragment)
                 true
             }
-            else -> super.onOptionsItemSelected(item)
+            else -> false
         }
     }
 
@@ -130,11 +130,11 @@ class CharactersFragment : Fragment(),
         return true
     }
 
-    override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+    override fun onMenuItemActionExpand(item: MenuItem): Boolean {
         return true
     }
 
-    override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+    override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
         viewModel.closeSearch()
         viewModel.searchCharacters()
         return true
